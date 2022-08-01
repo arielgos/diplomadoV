84e2b0cf8160213953b5554aa94080cfdf92a397
Ariel Ortuño Solis <arielg.os@gmail.com>
2022-08-01 18:57:27 -0400

    Samples y Events

--- public/assets/app.js
+++ public/assets/app.js
@@ -15,11 +15,11 @@ const firebaseConfig = {
  * Fix for invalid domain ans session atributes
  */
 window.dataLayer = window.dataLayer || [];
-window.gtag = function(){window.dataLayer.push(arguments);}
+window.gtag = function () { window.dataLayer.push(arguments); }
 
 window.gtag("config", firebaseConfig.measurementId, {
-	cookie_domain: window.location.hostname,
-	cookie_flags: "SameSite=None;Secure",
+    cookie_domain: window.location.hostname,
+    cookie_flags: "SameSite=None;Secure",
 });
 
 const firebase = initializeApp(firebaseConfig);
@@ -28,13 +28,37 @@ const analytics = getAnalytics(firebase);
 //analytics
 logEvent(analytics, 'App Start');
 
+let trackSimpleEventButton = document.getElementById("trackSimpleEvent");
+
+trackSimpleEventButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    logEvent(analytics, 'Track Simple Event Click');
+    console.log("Evento rastreado");
+});
+
 let trackEventButton = document.getElementById("trackEvent");
 
 trackEventButton.addEventListener('click', function (event) {
     event.preventDefault();
-    logEvent(analytics, 'Track Event Click');
-    console.log("Evento rastreado");
+
+    const params = {
+        item_list_id: 'P001',
+        item_list_name: 'Products',
+        items: [{
+            item_id: 'SKU_123',
+            item_name: 'Laptop 1',
+            item_category: 'PC',
+            item_variant: 'black',
+            item_brand: 'Apple',
+            price: 2350
+        }]
+    };
+
+    logEvent(analytics, 'Track Event Click', params);
+
+    console.log("Evento con parametros rastreado");
 });
+
 
 
 
