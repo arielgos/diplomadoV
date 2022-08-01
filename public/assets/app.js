92a0bbaf6bc55a1b703633f8de4fd334c09343d7
Ariel Ortuño Solis <arielg.os@gmail.com>
2022-08-01 19:16:09 -0400

    Remote Config

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
 import { getAnalytics, logEvent, setUserProperties } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js";
+import { getRemoteConfig, getValue, fetchAndActivate } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-remote-config.js";
 
-const firebaseConfig = {
+const config = {
     apiKey: "AIzaSyCN434D3q_QIE1yDFf4QNtiKRDp93mA3fs",
     authDomain: "diplomadov-f5e39.firebaseapp.com",
     projectId: "diplomadov-f5e39",
@@ -17,13 +18,14 @@ const firebaseConfig = {
 window.dataLayer = window.dataLayer || [];
 window.gtag = function () { window.dataLayer.push(arguments); }
 
-window.gtag("config", firebaseConfig.measurementId, {
+window.gtag("config", config.measurementId, {
     cookie_domain: window.location.hostname,
     cookie_flags: "SameSite=None;Secure",
 });
 
-const firebase = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebase);
+const app = initializeApp(config);
+const analytics = getAnalytics(app);
+const remoteConfig = getRemoteConfig(app);
 
 //analytics
 logEvent(analytics, 'App Start');
@@ -62,5 +64,23 @@ trackEventButton.addEventListener('click', function (event) {
 });
 
 
+/**
+ * Remote config
+ */
+remoteConfig.settings.minimumFetchIntervalMillis = 3600;
 
+const rcDefaultsFile = await fetch('remote_config_defaults.json');
+const rcDefaultsJson = await rcDefaultsFile.json();
 
+remoteConfig.defaultConfig = rcDefaultsJson;
+
+console.log(remoteConfig.defaultConfig.welcomeMessage);
+
+fetchAndActivate(remoteConfig)
+    .then(() => {
+        const welcomeMessage = getValue(remoteConfig, "welcomeMessage");
+        console.log(welcomeMessage.asString());
+    })
+    .catch((err) => {
+        console.error(err);
+    });
