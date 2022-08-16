11e90fd8036e641bdb0191bbc413a047fc639070
Ariel Ortuño Solis <arielg.os@gmail.com>
2022-08-15 21:02:34 -0400

    Adding moment Js

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,12 @@
 const functions = require("firebase-functions");
+const moment = require("moment");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
+exports.holaMundo = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!" + moment().format(), { structuredData: true });
+    response.send("Hello from Firebase! " + moment().format());
 });
 
 
