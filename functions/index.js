308b6cbe39f6f3b953c50f2a817bbaf42d05588a
Ariel Ortuno Solis <arielsolis@sherpacrm.com>
2022-08-22 17:37:07 -0400

    Resize Image

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require("firebase-functions");
 const moment = require("moment");
 const admin = require("firebase-admin");
+const spawn = require("child-process-promise").spawn;
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
 admin.initializeApp();
 
@@ -25,4 +29,32 @@ exports.newImage = functions.storage.object().onFinalize(() => {
         }).catch((error) => {
             console.log("Notification sent failed:", error);
         });
+});
+
+
+exports.resizeImage = functions.storage.object().onFinalize(async (object) => {
+    const filePath = object.name;
+    const fileName = path.basename(filePath);
+    if (!fileName.startsWith("thumb_")) {
+        const fileBucket = object.bucket;
+        const bucket = admin.storage().bucket(fileBucket);
+        const tempFilePath = path.join(os.tmpdir(), fileName);
+        const metadata = {
+            contentType: object.contentType,
+        };
+        await bucket.file(filePath).download({ destination: tempFilePath });
+        functions.logger.log("Image downloaded locally to", tempFilePath);
+        // Generate a thumbnail using ImageMagick.
+        await spawn("convert", [tempFilePath, "-thumbnail", "200x200>", tempFilePath]);
+        functions.logger.log("Thumbnail created at", tempFilePath);
+        const thumbFileName = `thumb_${fileName}`;
+        const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+        await bucket.upload(tempFilePath, {
+            destination: thumbFilePath,
+            metadata: metadata,
+        });
+        return fs.unlinkSync(tempFilePath);
+    } else {
+        return console.log('Already a Thumbnail.');
+    }
 });
